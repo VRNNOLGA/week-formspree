c7c9b11fa51610f379bd6ae1e7a27e5f7c33f6c3
VRNNOLGA <olga-pekin@yandex.ru>
2023-08-28 21:59:06 +0200

    create new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from '@formspree/react';
+//import React from 'react';
+//import ContactForm from './ContactForm';
+
+//export default function App() {
+ // return (
+    //<div>
+      //<ContactForm />
+    //</div>
+  //);
+//}
+
 
 function App() {
+  const [state, handleSubmit] = useForm('{https://formspree.io/f/xjvzpngy}');
+  if (state.succeeded) {
+    return <div>Thank you for signing up!</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form 
+    onSubmit={handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input id="email" type="email" name="email" />
+      <label>
+    Your message:
+    <textarea class="text" name="message" placeholder="Type your message..." required =""></textarea>
+  </label>
+      <button type="submit" disabled={state.submitting}>Sign up</button>
+    </form>
+  )
 }
-
 export default App;
+
